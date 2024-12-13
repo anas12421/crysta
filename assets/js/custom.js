15c0cee14022bfe71dd5c1d9084df88312a548fd
anas12421 <anas161090@gmail.com>
2024-12-13 12:05:55 +0600

    demo

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,22 @@ $(function () {
 
   "use strict"
 
-  
+  // Backtotop JS
+  $(".backtotop i").on('click', function () {
+    $("html,body").animate({
+      scrollTop: 0
+    }, 1200)
+  })
+
+  $(window).on('scroll', function () {
+    let scrolling = $(this).scrollTop()
+
+    if (scrolling > 20) {
+      $(".backtotop i").fadeIn(500)
+    } else {
+      $(".backtotop i").fadeOut(500)
+    }
+  })
+
 
 })
